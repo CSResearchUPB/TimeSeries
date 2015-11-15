65dbc18589ddfa4875f4c80f0149fc6e3e2b59aa
Dan Huru <danhuru@yahoo.com>
2015-11-15 16:37:33 +0200

    [kafkatreamer] send message to remote broker OK

---
+++ @kafkastreamer/src/main/java/Producer.java
@@ -0,0 +1,31 @@
+import java.util.Properties;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+public class Producer {
+
+	public static void main(String[] args) {
+
+		Properties props = new Properties();
+
+		props.put("bootstrap.servers", "192.168.0.114:9092");
+		props.put("key.serializer", StringSerializer.class);
+		props.put("value.serializer", StringSerializer.class);
+
+		KafkaProducer<String, String> producer = new KafkaProducer<String, String>(
+				props);
+
+		ProducerRecord<String, String> record = new ProducerRecord<String, String>(
+				"test", "java remote test 3");
+
+		producer.send(record);
+
+		System.out.println("Sent message" + record);
+
+		producer.close();
+
+	}
+
+}
