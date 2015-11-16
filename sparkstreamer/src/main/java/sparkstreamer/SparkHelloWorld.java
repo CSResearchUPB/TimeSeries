f213f4d60258d1a3cf1d66abe26abe374503d1c9
Dan Huru <danhuru@yahoo.com>
2015-11-16 21:58:25 +0200

    [sparkstreamer] working dstream

--- sparkstreamer/src/main/java/sparkstreamer/SparkHelloWorld.java
+++ sparkstreamer/src/main/java/sparkstreamer/SparkHelloWorld.java
@@ -8,7 +8,9 @@ import java.util.Set;
 import kafka.serializer.StringDecoder;
 
 import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.streaming.Durations;
+import org.apache.spark.streaming.api.java.JavaPairInputDStream;
+import org.apache.spark.streaming.api.java.JavaStreamingContext;
 import org.apache.spark.streaming.kafka.KafkaUtils;
 
 public class SparkHelloWorld {
@@ -16,19 +18,25 @@ public class SparkHelloWorld {
 	public static void main(String[] args) {
 
 		SparkConf conf = new SparkConf().setAppName("spark").setMaster("local");
-		JavaSparkContext sc = new JavaSparkContext(conf);
+
+		JavaStreamingContext jssc = new JavaStreamingContext(conf,
+				Durations.seconds(2));
+		
+		
 
 		System.out.println("Running spark");
 
 		Set<String> topics = new HashSet<>();
 		Map<String, String> kafkaParams = new HashMap<>();
 		topics.add("test");
-		kafkaParams.put("metadata.broker.list", "192.168.0.114:9092");
+		kafkaParams.put("metadata.broker.list", "192.168.0.118:9092");
 
-		JavaPairReceiverInputDStream<String, String> directKafkaStream = KafkaUtils
-				.createDirectStream(sc, String.class, String.class,
+		JavaPairInputDStream<String, String> messages = KafkaUtils
+				.createDirectStream(jssc, String.class, String.class,
 						StringDecoder.class, StringDecoder.class, kafkaParams,
 						topics);
 
+		System.out.println(messages);
+		
 	}
 }
