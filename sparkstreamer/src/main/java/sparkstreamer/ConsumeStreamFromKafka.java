f0b36f55aade6ea5b668a43cdb9141fa9231e7a3
Dan Huru <danhuru@yahoo.com>
2015-11-30 13:19:58 +0100

    [sparkstreamer] write in hbase ok

--- sparkstreamer/src/main/java/sparkstreamer/ConsumeStreamFromKafka.java
+++ sparkstreamer/src/main/java/sparkstreamer/ConsumeStreamFromKafka.java
@@ -4,28 +4,16 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
-import java.util.regex.Pattern;
 
 import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.function.FlatMapFunction;
-import org.apache.spark.api.java.function.Function;
-import org.apache.spark.api.java.function.Function2;
-import org.apache.spark.api.java.function.PairFunction;
 import org.apache.spark.streaming.Durations;
-import org.apache.spark.streaming.api.java.JavaDStream;
-import org.apache.spark.streaming.api.java.JavaPairDStream;
 import org.apache.spark.streaming.api.java.JavaPairInputDStream;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
 import org.apache.spark.streaming.kafka.KafkaUtils;
 
-import com.google.common.collect.Lists;
-
 import kafka.serializer.StringDecoder;
-import scala.Tuple2;
 
 public class ConsumeStreamFromKafka {
-
-	private static final Pattern SPACE = Pattern.compile(" ");
 
 	public static void main(String[] args) {
 
@@ -43,30 +31,7 @@ public class ConsumeStreamFromKafka {
 		JavaPairInputDStream<String, String> messages = KafkaUtils.createDirectStream(jssc, String.class, String.class,
 				StringDecoder.class, StringDecoder.class, kafkaParams, topics);
 
-		JavaDStream<String> lines = messages.map(new Function<Tuple2<String, String>, String>() {
-			@Override
-			public String call(Tuple2<String, String> tuple2) {
-				return tuple2._2();
-			}
-		});
-		JavaDStream<String> words = lines.flatMap(new FlatMapFunction<String, String>() {
-			@Override
-			public Iterable<String> call(String x) {
-				return Lists.newArrayList(SPACE.split(x));
-			}
-		});
-		JavaPairDStream<String, Integer> wordCounts = words.mapToPair(new PairFunction<String, String, Integer>() {
-			@Override
-			public Tuple2<String, Integer> call(String s) {
-				return new Tuple2<String, Integer>(s, 1);
-			}
-		}).reduceByKey(new Function2<Integer, Integer, Integer>() {
-			@Override
-			public Integer call(Integer i1, Integer i2) {
-				return i1 + i2;
-			}
-		});
-		wordCounts.print();
+		messages.print();
 
 		// Start the computation
 		jssc.start();
--- sparkstreamer/src/main/java/sparkstreamer/ReadFromKafkaWriteToHbase.java
+++ sparkstreamer/src/main/java/sparkstreamer/ReadFromKafkaWriteToHbase.java
@@ -1,8 +1,10 @@
 package sparkstreamer;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.regex.Pattern;
@@ -18,25 +20,17 @@ import org.apache.hadoop.hbase.client.Result;
 import org.apache.hadoop.hbase.client.Table;
 import org.apache.hadoop.hbase.util.Bytes;
 import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.function.FlatMapFunction;
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.function.Function;
-import org.apache.spark.api.java.function.Function2;
-import org.apache.spark.api.java.function.PairFunction;
 import org.apache.spark.streaming.Durations;
-import org.apache.spark.streaming.api.java.JavaDStream;
-import org.apache.spark.streaming.api.java.JavaPairDStream;
 import org.apache.spark.streaming.api.java.JavaPairInputDStream;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
 import org.apache.spark.streaming.kafka.KafkaUtils;
-
-import com.google.common.collect.Lists;
 
 import kafka.serializer.StringDecoder;
 import scala.Tuple2;
 
 public class ReadFromKafkaWriteToHbase {
-
-	private static final Pattern SPACE = Pattern.compile(" ");
 
 	public static void main(String[] args) throws IOException {
 
@@ -59,7 +53,7 @@ public class ReadFromKafkaWriteToHbase {
 
 		SparkConf sparkConf = new SparkConf().setAppName("spark").setMaster("local");
 
-		JavaStreamingContext jssc = new JavaStreamingContext(sparkConf, Durations.seconds(10));
+		JavaStreamingContext jssc = new JavaStreamingContext(sparkConf, Durations.seconds(5));
 
 		Set<String> topics = new HashSet<>();
 		Map<String, String> kafkaParams = new HashMap<>();
@@ -69,18 +63,28 @@ public class ReadFromKafkaWriteToHbase {
 		JavaPairInputDStream<String, String> messages = KafkaUtils.createDirectStream(jssc, String.class, String.class,
 				StringDecoder.class, StringDecoder.class, kafkaParams, topics);
 
-		JavaDStream<String> lines = messages.map(new Function<String,String>() {
+		messages.foreachRDD(new Function<JavaPairRDD<String, String>, Void>() {
+
 			@Override
-			public String call(String a) {
-				return a;
+			public Void call(JavaPairRDD<String, String> v1) throws Exception {
+
+				int i = 0;
+
+				List<Put> newRows = new ArrayList<>();
+
+				for (Tuple2<String, String> tuple : v1.collect()) {
+
+					Put put = new Put(Bytes.toBytes("row" + (i++)));
+					put.addColumn(Bytes.toBytes("cf"), Bytes.toBytes("a"), Bytes.toBytes(tuple.toString()));
+					newRows.add(put);
+
+				}
+
+				testTable.put(newRows);
+
+				return null;
 			}
 		});
-
-		for (String string : words) {
-			Put put = new Put(Bytes.toBytes("row6"));
-			put.addColumn(Bytes.toBytes("cf"), Bytes.toBytes("a"), Bytes.toBytes("bau"));
-			testTable.put(put);
-		}
 
 		// Start the computation
 		jssc.start();
