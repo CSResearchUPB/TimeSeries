e038a2a0e8ea100749d7359336b33e0dd6659a08
Dan Huru <danhuru@yahoo.com>
2016-04-03 13:14:48 +0200

    working linear regression model, results not good yet

--- @sparkservice/src/main/java/sparkjobs/ProcessHistoricalData.java
+++ @sparkservice/src/main/java/sparkjobs/ProcessHistoricalData.java
@@ -1,16 +1,20 @@
 package sparkjobs;
 
-
 import java.io.IOException;
 
-import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.hbase.HBaseConfiguration;
-import org.apache.hadoop.hbase.TableName;
-import org.apache.hadoop.hbase.client.Connection;
-import org.apache.hadoop.hbase.client.ConnectionFactory;
-import org.apache.hadoop.hbase.client.Table;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.ml.feature.MinMaxScaler;
+import org.apache.spark.ml.feature.MinMaxScalerModel;
+import org.apache.spark.ml.feature.PCA;
+import org.apache.spark.ml.feature.PCAModel;
+import org.apache.spark.ml.feature.PolynomialExpansion;
+import org.apache.spark.ml.regression.LinearRegression;
+import org.apache.spark.ml.regression.LinearRegressionModel;
+import org.apache.spark.ml.regression.LinearRegressionTrainingSummary;
+import org.apache.spark.mllib.linalg.Vectors;
+import org.apache.spark.sql.DataFrame;
+import org.apache.spark.sql.SQLContext;
 
 public class ProcessHistoricalData {
 
@@ -18,21 +22,75 @@ public class ProcessHistoricalData {
 
 	public static void main(String[] args) throws IOException {
 
-		Configuration conf = null;
-
-		conf = HBaseConfiguration.create();
-		conf.set("hbase.zookeeper.quorum", "localhost");
-		conf.set("hbase.zookeeper.property.clientPort", "2182");
-
-		Connection connection = ConnectionFactory.createConnection(conf);
-
-		Table testTable = connection.getTable(TableName.valueOf("sensordata"));
-
 		SparkConf sparkConf = new SparkConf().setAppName("spark").setMaster("local");
 
 		JavaSparkContext jsc = new JavaSparkContext(sparkConf);
+
+		SQLContext sqlContext = new SQLContext(jsc);
+
+		// $example on$
+		// Load training data
+		DataFrame training = sqlContext.read().format("libsvm").load("src/main/resources/linear_regression_temperature_data.txt");
+
+		training.show(false);
+		training.printSchema();
+
+		MinMaxScaler scaler = new MinMaxScaler()
+		  .setInputCol("features")
+		  .setOutputCol("scaledFeatures");
+
+		// Compute summary statistics and generate MinMaxScalerModel
+		MinMaxScalerModel scalerModel = scaler.fit(training);
+
+		// rescale each feature to range [min, max].
+		DataFrame scaledData = scalerModel.transform(training);
+		scaledData.show(false);
+		
+		PCAModel pca = new PCA()
+				  .setInputCol("scaledFeatures")
+				  .setOutputCol("pcaFeatures")
+				  .setK(5)
+				  .fit(scaledData);
+
+		DataFrame pcaResults = pca.transform(scaledData);
+		pcaResults.show(false);
+		
+		PolynomialExpansion polyExpansion = new PolynomialExpansion()
+				  .setInputCol("pcaFeatures")
+				  .setOutputCol("polyFeatures")
+				  .setDegree(2);
+		
+		DataFrame polyDF = polyExpansion.transform(pcaResults);
+		
+		polyDF.show(false);
+		
+		LinearRegression lr = new LinearRegression().setMaxIter(100).setRegParam(0.3).setElasticNetParam(0.8);
+
+		// Fit the model
+		LinearRegressionModel lrModel = lr.fit(polyDF);
+
+		// Print the coefficients and intercept for linear regression
+		System.out.println("Coefficients: " + lrModel.coefficients() + " Intercept: " + lrModel.intercept());
+
+		// Summarize the model over the training set and print out some metrics
+		LinearRegressionTrainingSummary trainingSummary = lrModel.summary();
+		System.out.println("numIterations: " + trainingSummary.totalIterations());
+		System.out.println("objectiveHistory: " + Vectors.dense(trainingSummary.objectiveHistory()));
+		trainingSummary.residuals().show();
+		System.out.println("RMSE: " + trainingSummary.rootMeanSquaredError());
+		System.out.println("r2: " + trainingSummary.r2());
+		// $example off$
+
+		lrModel.transform(polyDF).show(false);
+		
+		System.out.println(trainingSummary.predictions());
+
+		System.out.println(lrModel.explainParams());
+		
 		
 		jsc.stop();
+
+		System.out.println("bau");
 
 	}
 
